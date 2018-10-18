3882c16a7281d2c8264c2ebfdf408430c2bedab3
curtisburns <curtis.burns28@gmail.com>
2018-10-18 23:12:13 +0100

    added form validation using vee-validate

--- vue-skills/src/components/Skills.vue
+++ vue-skills/src/components/Skills.vue
@@ -2,7 +2,8 @@
   <div class="hello">
     <div class="holder">
     <form @submit.prevent="addSkill">
-      <input type="text" placeholder="Enter your skill here" v-model="skill">
+      <input type="text" placeholder="Enter your skill here" v-model="skill" v-validate="'min:5'" name="skill">
+      <p class="alert" v-if="errors.has('skill')">{{ errors.first('skill') }}</p>
     </form>
       <ul>
         <li v-for="(data, index) in skills" :key=index>{{ data.skill }}</li>
@@ -68,5 +69,22 @@ export default {
 
  .container {
    box-shadow: 0px 0px 40px lightgray;
+ }
+
+ input {
+    width: calc(100% - 40px);
+    border: 0;
+    padding: 20px;
+    font-size: 1.3em;
+    background-color: #323333;
+    color: #687F7F;
+  }
+
+  .alert {
+   background: #fdf2ce;
+   font-weight: bold;
+   display: inline-block;
+   padding: 5px;
+   margin-top: -20px;
  }
 </style>
